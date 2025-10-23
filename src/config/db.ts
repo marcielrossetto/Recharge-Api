8edd2c3e8e2a598e5cc57acc4d29a5494698db0a
marcielrossetto <40443010+marcielrossetto@users.noreply.github.com>
2025-10-22 21:18:45 -0300

    Correcao var  Banco de dados Render

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,21 +1,29 @@
+// src/config/db.ts
 import pg from "pg";
 import dotenv from "dotenv";
 dotenv.config();
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"; // DEV somente
-
 const { Pool } = pg;
+
 const db = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
+  ssl: { rejectUnauthorized: false }
 });
 
-export async function query<T extends pg.QueryResultRow = any>(text: string, params?: any[]) {
-  const res = await db.query<T>(text, params as any);
+export async function query<T extends pg.QueryResultRow = any>(
+  text: string,
+  params?: any[]
+) {
+  const res = await db.query<T>(text, params);
   return res.rows;
 }
-export async function queryOne<T extends pg.QueryResultRow = any>(text: string, params?: any[]) {
+
+export async function queryOne<T extends pg.QueryResultRow = any>(
+  text: string,
+  params?: any[]
+) {
   const rows = await query<T>(text, params);
   return rows[0] ?? null;
 }
+
 export default db;
